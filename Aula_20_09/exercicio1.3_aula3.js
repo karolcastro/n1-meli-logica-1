75f00402386f8544d9fe0143c2f9735daaff370d
karolcosta <karolinycosta@yahoo.com.br>
2019-12-12 22:32:34 -0300

    teste

---
+++ Aula_20_09/exercicio1.3_aula3.js
@@ -0,0 +1,1 @@
+//  1.3 não restrinja a entrada de dados a apenas 5 atletas. Agora após a inclusão de cada atleta o programa deve perguntar ao usuário se deseja incluir outro. A resposta é dada com (S/N). O programa continua mostrando o nome do mais alto, e a altura média.
